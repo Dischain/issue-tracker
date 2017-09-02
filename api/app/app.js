80bdac698c7bd7730964f0e252061b18a9f7e5ca
Aleksands Shaytanov <rdischain@yandex.ru>
2017-09-03 02:34:36 +0400

    add express sessions

--- api/app/app.js
+++ api/app/app.js
@@ -1,13 +1,23 @@
-const app = require('express')();
+const express = require('express');
 const bodyParser  = require('body-parser');
+const express_session = require('express-session')
+const config = require('./config');
+const passport = require('./auth');
 
 const issueRouter = require('./routes/issue.js');
-const config = require('./config');
+const userRouter = require('./routes/user.js');
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.cookieParser());
+app.use(express_session({ secret: 'keyboard cat' }));
+app.use(passport.initialize())
+app.use(passport.session());
 
 app.use('/', issueRouter);
+app.use('/', userRouter);
 
 app.use((req, res, next) => {
   res.status(404);
@@ -17,3 +27,5 @@ app.use((req, res, next) => {
 app.listen(config.app.port, () => {
   console.log('server listening on port ' + config.app.port);
 });
+
+module.exports = app;
