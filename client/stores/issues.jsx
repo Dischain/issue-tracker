97e0a2a1e69692c1595da789c082248ba82c05aa
Aleksands Shaytanov <rdischain@yandex.ru>
2017-09-19 01:12:58 +0400

    update

--- client/stores/issues.jsx
+++ client/stores/issues.jsx
@@ -5,6 +5,7 @@ import Reflux from 'reflux';
 import IssuesAction from'../actions/issues.jsx';
 
 let _issues = [];
+// let _issues = { issues: [], metadata: null };
 
 const IssuesStore = Reflux.createStore({
   init() {
