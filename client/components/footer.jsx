0a041d691f0c0975cb459ed10bbbd52159f177bc
Aleksands Shaytanov <rdischain@yandex.ru>
2017-09-20 22:24:04 +0400

    add footer component

---
+++ client/components/footer.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import { Row } from 'react-bootstrap';
+
+const Footer = React.createClass({
+  render() {
+    return (
+      <foooter className={'footer'}>
+        <p className={'text-muted'}>Issue Tracker, 2017</p>
+      </foooter>
+    );
+  }
+});
+
+export default Footer;
